0adb686014319fbec1b7b3f7bbf8cdae8895795c
rubytaiki <taikimalay@gmail.com>
2021-08-07 17:03:11 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :island
+
+  with_options presence: true do
+    validates :nickname
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+    end
+  end
+
+  validates :island_id, numericality: { other_than: 1, message: "can't be blank" }
+       
+end
