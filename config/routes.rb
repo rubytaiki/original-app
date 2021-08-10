6c6a9635fd50c11f0e347f305dcc82e29d29f02e
rubytaiki <taikimalay@gmail.com>
2021-08-10 17:06:35 +0900

    対馬壱岐フォト紹介ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
-  resources :homes, only: :index
+  resources :foods
+  resources :users, only: :index
+  resources :homes, only: :index do
+    collection do
+      get 'tushima'
+      get 'iki'
+    end
+  end
 end
