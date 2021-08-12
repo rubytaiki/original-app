e3f47cee1ff98e97c8c4ad7e552684a502308ec6
rubytaiki <taikimalay@gmail.com>
2021-08-12 20:30:11 +0900

    追加マージ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
+<<<<<<< Updated upstream
   resources :homes, only: :index
+=======
+  resources :foods,  only: [:new, :create, :edit, :update, :destroy]
+  resources :hotels, only: [:new, :create, :edit, :update, :destroy]
+  resources :users,  only: :index
+  resources :homes,  only: :index do
+    collection do
+      get 'tushima'
+      get 'iki'
+      get 'eat'
+      get 'stay'
+    end
+  end
+>>>>>>> Stashed changes
 end
