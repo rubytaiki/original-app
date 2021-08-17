be8368abe769b28d5f2406db51d54f378439adf8
rubytaiki <taikimalay@gmail.com>
2021-08-17 23:41:41 +0900

    Payjp導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require('jquery')
 require("channels")
 require("slider.js")
 require("gallery.js")
+require("purchese.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
